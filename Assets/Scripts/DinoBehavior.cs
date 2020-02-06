258301ac6e3fd3f18a2e42d63519b038a6318dab
MatteoVera001 <MatteoVera001@gmail.com>
2020-02-06 17:14:11 -0600

    Added Dino Movement and Health

---
+++ Assets/Scripts/DinoBehavior.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DinoBehavior : MonoBehaviour
+{
+    public Health hlth;
+    public Movement mvmnt;
+    public SpriteRenderer sr;
+    public Rigidbody2D rb2d;
+    public GameManager gm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d.freezeRotation = true;
+        if (gameObject.transform.position.x>0)
+        {
+            mvmnt.Dir.x = -1;
+            
+        }
+        else
+        {
+            sr.flipX = true;
+        }
+        mvmnt.StartMoving();
+
+        sr.sortingOrder = Mathf.CeilToInt((gameObject.transform.position.y*-1) +5);
+
+        //Probably a better way to do this.
+        gm = GameObject.Find("ControllerObject").GetComponent<GameManager>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDestroy()
+    {
+        gm.EnemiesLeftAlive -= 1;
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Vector2 Dir = new Vector2(1,0);
+    public float MoveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Maybe we should have the dino move up and down a little bit procedurally, like a walk cycle.
+    }
+
+    public void StartMoving()
+    {
+        GetComponent<Rigidbody2D>().velocity = Dir * MoveSpeed;
+    }
+
+    public void StopMoving()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2();
+    }
+}
