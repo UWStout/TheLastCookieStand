0c06391a8434b5a4bfb5137f1322c1ded70815a4
MatteoVera001 <MatteoVera001@gmail.com>
2020-02-10 16:27:53 -0600

    Pause Functionallity

---
+++ Assets/Scripts/PauseManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PauseManager : MonoBehaviour
+{
+    public bool isPaused;
+    public GameObject pauseMenu;
+    public Image pauseButton;
+    public Sprite pauseSprite;
+    public Sprite playSprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Pause()
+    {
+        Image img;
+        if (isPaused)
+        {
+            pauseButton.sprite=pauseSprite;
+            isPaused = false;
+            pauseMenu.SetActive(false);
+            Time.timeScale = 1;
+        }
+        else
+        {
+            pauseButton.sprite=playSprite;
+            isPaused = true;
+            Time.timeScale = 0;
+            pauseMenu.SetActive(true);
+        }
+    }
+}
